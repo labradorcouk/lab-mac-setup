28c461085ac0d0d77d7fa2fba132c41f3c97b601
Antonio Terreno <aterreno@users.noreply.github.com>
2018-11-07 11:36:05 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,78 +7,58 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 #bare min
 
-brew install --HEAD concourse/tap/fly
 brew install ag
 brew install autojump
 brew install awscli
 brew install base64
 brew install ccat
-brew install certbot
-brew install chromedriver
 brew install code
-brew install cw
-brew install docker
-brew install dockutil
-brew install dot
 brew install firefox
 brew install fly
-brew install fzf
 brew install git
 brew install gpgsuite
-brew install graphviz
-brew install hub
-brew install jid
 brew install jq
-brew install jump
-brew install libsass
-brew install macvim
-brew install mysql
-brew install nvm
-brew install openvpn
-brew install packer
-brew install postgres
-brew install pre-commit
-brew install python3
-brew install python
-brew install redis
-brew install ruby
-brew install s3cmd
-brew install sslmate
-brew install tfenv
-brew install tflint
-brew install tmux
 brew install tree
 brew install watch
 brew install wget
+brew install autojump
+brew install openvpn
+brew install python pip virtualenv
+brew install virtualenv
 
-nvm install node
+nvm install v8.10.0
 tfenv install 0.11.2
 
-brew cask install caffeine
-brew cask install charles
-brew cask install chicken
+brew cask install authy
 brew cask install chrome
 brew cask install cyberduck
-brew cask install docker
-brew cask install dropbox
-brew cask install evernote
 brew cask install firefox
 brew cask install google-chrome
-brew cask install gpgsuite
-brew cask install gpgtools
-brew cask install iterm2
 brew cask install keyboard-cleaner
-brew cask install numi
-brew cask install postman
+brew cask install virtualbox
+brew cask install caffeine
+brew cask install dropbox
+brew cask install firefox
+brew cask install iterm2
 brew cask install slack
 brew cask install spectacle
-brew cask install sublime-text
-brew cask install teamviewer
+brew cask install spotify
 brew cask install the-unarchiver
-brew cask install tunnelblick
-brew cask install vagrant
 brew cask install virtualbox
-brew cask install webstorm
+brew cask install tunnelblick
+brew install dockutil
+brew install git
+brew install autojump
+brew install jq
+brew install wget
+brew install macvim
+brew install nvm
+brew install gpg2
+brew install tfenv
+brew cask install dropbox
+brew cask install nordvpn
+brew cask install skype
+brew cask install visual-studio-code
 
 dockutil --remove all
 
